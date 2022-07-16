64e28690ba635620387e01146f916cb7d075f0fa
ahmedGad-dev <76513984+ahmedGad-dev@users.noreply.github.com>
2022-07-16 15:08:23 +0200

    Simple React App

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+
+//the Set method neglects the duplicate values
+const allCategories = ['All', ...new Set(items.map(item => item.category))]
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items)
+  const [categories, setCategories] = useState(allCategories)
+
+  const filterItems = (category) => {
+    if(category === 'All'){
+        setMenuItems(items)
+        return
+    }
+    let newItems = items.filter((item) => {
+        return item.category === category
+    })
+    setMenuItems(newItems)
+  }
+
+
+  return(
+    <main>
+     <section className="menu section">
+      <div className="title">
+        <h2>our menu</h2>
+        <div className="underline"></div>
+      </div>
+      <Categories categories={categories} filterItems={filterItems} />
+      <Menu items={menuItems} />
+    </section>
+  </main>
+  );
+}
+
+export default App;
